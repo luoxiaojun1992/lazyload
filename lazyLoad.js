0f2013e96b7cd8653a7b66223398ebbac3822e2b
罗晓俊 <luoxiaojun@angejia.com>
2016-08-01 13:39:45 +0800

    optimize

--- lazyLoad.js
+++ lazyLoad.js
@@ -1,51 +1,53 @@
-(function($)
-{
+(function($) {
 
-	$.fn.Lazyload = function(ops)
-	{
-		ops = $.extend({
-			'enable' : false,
-			'obj' : 'body'
-		},ops);
+	$.fn.extend{
+    lazyLoad: function(config) {
 
-		if(ops.enable) {
-			init();
-		}
+      //初始化配置
+		  var configuration = $.extend({
+			  'enable' : false,
+		  }, config);
 
-    function load()
-    {
+		  if(configuration.enable) {
+			  $(this).init();
+		  }
+    },
+
+    laod: function() {
+        var thisObj = this;
+
 				//获取当前窗口的可见高度
-				var windowHeigth = jQuery(window).height();
+				var windowHeigth = $(window).height();
 
 				//获取当前窗口的可见宽度
-				var windowWidth = jQuery(window).width();
-				jQuery(ops.obj).find('img').each(function(){
+				var windowWidth = $(window).width();
+				$(thisObj).each(function(){
 
 					//获取图片的src属性
-					var src = jQuery(this).attr('src');
+					var src = $(this).attr('src');
 
 					//替换图片src属性为src2属性，并移除src属性
-					jQuery(this).attr('src2',src).removeAttr('src');
+					$(this).attr('src2',src).removeAttr('src');
 
 					//获取图片的绝对位置
-					var offset = jQuery(this).offset();
-					if(offset.left >= jQuery(window).scrollLeft() && offset.top >= jQuery(window).scrollTop() && offset.left <= (jQuery(window).scrollLeft() + windowWidth) && offset.top <= (jQuery(window).scrollTop() + windowHeigth)){
-						jQuery(this).attr('src',src).removeAttr('src2').attr('alt','图片已加载！');
+					var offset = $(this).offset();
+					if(offset.left >= $(window).scrollLeft() && offset.top >= $(window).scrollTop() && offset.left <= ($(window).scrollLeft() + windowWidth) && offset.top <= ($(window).scrollTop() + windowHeigth)){
+						$(this).attr('src',src).removeAttr('src2').attr('alt','图片已加载！');
 					}
 				});
-    }
+    },
 
-		function init()
-		{
-			jQuery(document).ready(function(){
+    init: function() {
+			$(document).ready(function(){
         // 初始化加载图片
-		    load();
+		    $(this).load();
 			});
 
-			jQuery(window).on('scroll',function(){
+			$(window).on('scroll',function(){
         // 滚动加载图片
-			  load();
+			  $(this).load();
 			});
 		}
 	};
 })(jQuery)
+
